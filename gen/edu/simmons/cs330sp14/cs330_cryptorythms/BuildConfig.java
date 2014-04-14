4b7bd79b8d0caf50614f8b6b720fcceb9606ba61
Amber <amber.stubbs@gmail.com>
2014-04-13 21:52:28 -0400

    adding eclipse repository

---
+++ gen/edu/simmons/cs330sp14/cs330_cryptorythms/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.simmons.cs330sp14.cs330_cryptorythms;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
